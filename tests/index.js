9aaed2d84878806b29606fec1710096e01dd42d1
Ivan Kozik <ivan@ludios.org>
2015-05-13 21:10:50 +0000

    Fix tests for s/len/size/ change

--- tests/index.js
+++ tests/index.js
@@ -5,22 +5,22 @@ const inspect = require('util').inspect;
 describe('Buffer.prototype.inspect', function() {
 	it('shows 0-length Buffers', function() {
 		const buf = new Buffer(0);
-		assert.equal(inspect(buf), '<Buffer len=0>');
+		assert.equal(inspect(buf), '<Buffer size=0>');
 	});
 
 	it('shows undecodeable Buffers as bytes', function() {
 		const buf = new Buffer(4).fill(255);
-		assert.equal(inspect(buf), '<Buffer len=4: ff ff ff ff>');
+		assert.equal(inspect(buf), '<Buffer size=4: ff ff ff ff>');
 	});
 
 	it('shows decodeable but too-long Buffers as bytes', function() {
 		const buf = new Buffer(1000).fill(0);
-		assert.equal(inspect(buf), '<Buffer len=1000: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ... >');
+		assert.equal(inspect(buf), '<Buffer size=1000: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ... >');
 	});
 
 	it('shows decodeable Buffers as utf-8 strings', function() {
-		assert.equal(inspect(new Buffer("hello world\n")), "<Buffer len=12: utf8 'hello world\\n'>");
-		assert.equal(inspect(new Buffer(2).fill(0)), "<Buffer len=2: utf8 '\\u0000\\u0000'>");
-		assert.equal(inspect(new Buffer("\ucccc", "utf8")), "<Buffer len=3: utf8 '\ucccc'>");
+		assert.equal(inspect(new Buffer("hello world\n")), "<Buffer size=12: utf8 'hello world\\n'>");
+		assert.equal(inspect(new Buffer(2).fill(0)), "<Buffer size=2: utf8 '\\u0000\\u0000'>");
+		assert.equal(inspect(new Buffer("\ucccc", "utf8")), "<Buffer size=3: utf8 '\ucccc'>");
 	});
 });
